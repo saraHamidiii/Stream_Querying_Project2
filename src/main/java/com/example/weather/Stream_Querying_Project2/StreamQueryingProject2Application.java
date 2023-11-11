1128dd876d01fbc10db63030b02831a815183226
Drink-some-water <106268016+Drink-some-water@users.noreply.github.com>
2023-11-11 14:13:48 -0800

    making spring project

---
+++ src/main/java/com/example/weather/Stream_Querying_Project2/StreamQueryingProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.weather.Stream_Querying_Project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamQueryingProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamQueryingProject2Application.class, args);
+	}
+
+}
