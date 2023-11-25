54e18af66cca67015d4e8a79c5d7c93b6108d6b9
Sara Hamidi <Sara.Hamidi01@student.csulb.edu>
2023-11-24 18:30:52 -0800

    encoded json for url

--- src/main/java/com/example/weather/Stream_Querying_Project2/HTTPclient.java
+++ src/main/java/com/example/weather/Stream_Querying_Project2/HTTPclient.java
@@ -3,14 +3,18 @@ package com.example.weather.Stream_Querying_Project2;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class HTTPclient {
+
+    //http:localhost:8080/api/v1/kafka/publish?message=hello world
     private static String apiUrl = "http://localhost:8080/api/v1/kafka/publish";
 
     public static void connect(String message){
         try {
-            URL url = new URL(apiUrl + "?message=" + message);
+            String encodedMessage = URLEncoder.encode(message, StandardCharsets.UTF_8.toString());
+            URL url = new URL(apiUrl + "?message=" + encodedMessage);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             int responseCode = conn.getResponseCode();
